6be5f9537841b408b364e66cad940744d20f1ed0
tianlu1677 <tianlu1677@gmail.com>
2018-03-06 11:34:49 +0800

    add html parse

--- src/config.js
+++ src/config.js
@@ -1,12 +1,10 @@
 // 全局的配置文件
 // const env = 'development'
-// const env = 'test'
 const env = 'production'
 const version = 1.0
 
 const hosts = {
   development: 'http://localhost:4000',
-  test:        'https://niubibeta.com:8080',
   production:  'https://niubibeta.com'
 }
 
